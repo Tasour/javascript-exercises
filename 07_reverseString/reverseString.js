d7ec9b9c2b77d14afcfa8f4fa7c1f6211faf6ef7
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-22 23:23:01 +0200

    Solve 07-reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let r_text = ""
+    for (let index = 0; index < text.length; index++) {
+        r_text += text[text.length-1-index]
+    }
+    return r_text
 };
 
 // Do not edit below this line

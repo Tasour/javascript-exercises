3885971db7a0b09cbd530f76a0909b776b278d0e
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-22 23:18:30 +0200

    Solve 06_repeatStrings

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let end_text = "";
+    for (let index = 0; index < times; index++) {
+        end_text += text;
+    }
+    return end_text;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;

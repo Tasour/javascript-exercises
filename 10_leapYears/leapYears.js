c2f79e1773416ed133a955efffc7bdaa2fb257a2
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-23 09:20:13 +0200

    solve exercise 10 leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    //Les années sont bissextiles si elles sont multiples de quatre, 
+    // mais pas si elles sont multiples de cent, 
+    // à l'exception des années multiples de quatre cents qui, 
+    // elles, sont également bissextiles. 
+
+    return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
 
 };
 

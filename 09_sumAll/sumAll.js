eaed67030b0903bdf1a0bdf4ee69191f2df23fec
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-23 09:14:31 +0200

    solve 09 exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(b_inf, b_sup) {
+    if (b_inf > b_sup) {
+        [b_sup, b_inf] = [b_inf, b_sup]
+    }
+    if (!Number.isInteger(b_inf) || b_inf <= 0 || !Number.isInteger(b_sup) || b_sup <= 0) {
+        return "ERROR";
+    }
 
+    let nb_sumAll = 0
+    for (let index = b_inf; index <= b_sup; index++) {
+        nb_sumAll+=index
+    }
+    return nb_sumAll
 };
 
 // Do not edit below this line

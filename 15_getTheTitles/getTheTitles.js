332839330fdcad75ba2e9f32eb2192df21c4a145
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-23 17:31:29 +0200

    solve 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(object) {
 
+    let title_array = []
+    for (let index = 0; index < object.length; index++) {
+        title_array.push(object[index]['title'])
+    }
+    return title_array
 };
 
 // Do not edit below this line

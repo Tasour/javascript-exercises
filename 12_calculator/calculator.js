e3da9758f3778bdf570ad25b8aa5fdee0032a2e8
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-23 09:34:07 +0200

    solve 12 calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let cumul = 0
+	for (let index = 0; index < array.length; index++) {
+    cumul+=array[index];
+  }
+  return cumul
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let cumul = 1
+	for (let index = 0; index < array.length; index++) {
+    cumul*=array[index];
+  }
+  return cumul
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+
+  for (let i = 2; i <= number; i++) {
+    result = result * i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line

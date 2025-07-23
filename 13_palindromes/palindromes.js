46854f7b6fe09e61f077c8d966f39129de1dcc4b
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-23 10:04:55 +0200

    solve 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    input = input
+      .toLowerCase()
+      .replace(/[^a-z0-9]/g, ""); // Garde lettres et chiffres, enlève ponctuation et espaces
 
+    let len = input.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (input[i] !== input[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;

b7c418e858103c3231567bad4e17c8f9aa82cfd2
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-23 17:27:07 +0200

    solve exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(max_step) {
+//each number ( Fibonacci number ) is the sum of the two preceding numbers
+// 1 1 2 3 5 8 12 20 etc...
+if (max_step < 0) {
+    return "OOPS"
+}
+let array = [0]
+for (let index = 0; index < max_step; index++) {
+    if (array.length < 2){
+        array.push(1)
+    }
+    else {
+        array.push(array[array.length-1] + array[array.length-2])
+    }
+}
+return array[array.length - 1]
 };
 
 // Do not edit below this line
